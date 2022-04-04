6ebf5d73a15bae9e144484ea8fa8d29fc2d2c497
Gregoire Parant <g.parant@thecodingmachine.com>
2022-04-04 19:28:35 +0200

    Push configure and script MAP OITF

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,31 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
+/* #Manage premium door */
 
-WA.room.onEnterLayer('clockZone').subscribe(() => {
-    console.log('toto')
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
+const DOOR_CLOSED_LAYER_NAME = 'doorClosed';
+const DOOR_OPENED_LAYER_NAME = 'doorOpened';
 
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+const closeDoor = () => {
+    WA.room.showLayer(DOOR_CLOSED_LAYER_NAME);
+    WA.room.hideLayer(DOOR_OPENED_LAYER_NAME);
+}
+const openDoor = () => {
+    WA.room.hideLayer(DOOR_CLOSED_LAYER_NAME);
+    WA.room.showLayer(DOOR_OPENED_LAYER_NAME);
+}
+const isPremium = () => {
+    return WA.player.tags.includes('premium') || WA.player.tags.includes('Premium') 
+    || WA.player.tags.includes('PREMIUM') || WA.player.tags.includes('editor');
+}
+function toggleDoor() {
+    console.log('isPremium()', isPremium());
+    if(isPremium()){
+        openDoor();
+    }else{
+        closeDoor();
     }
 }
+
+closeDoor();
+WA.room.onEnterLayer('doorZone').subscribe(toggleDoor);
