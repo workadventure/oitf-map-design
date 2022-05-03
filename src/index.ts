2823c0c077e7879caa3225b99f70cb5348429580
Gregoire Parant <g.parant@thecodingmachine.com>
2022-05-03 16:36:01 +0200

    Update properties and layer define

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,19 @@
 
 import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 
+const VIDEO_LAYER = 'Video';
+const DOCUMENT_LAYER = 'Document';
+const PANNEAU_LAYER = 'urlPanneau';
+const EXIT_CHAPITRE_TO_CERCLE_LAYER_PUBLIC = 'openWebsitEexitChapitreToCerclePublic';
+const EXIT_CHAPITRE_TO_CERCLE_LAYER_PRIVATE = 'openWebsitEexitChapitreToCerclePrivate';
+const EXIT_CERCLE_TO_CHAPITRE_LAYER_PUBLIC = 'openWebsitEexitCercleToChapitrePublic';
+const EXIT_CERCLE_TO_CHAPITRE_LAYER_PRIVATE = 'openWebsitEexitCercleToChapitrePrivate';
+const EXIT_CERCLE_TO_SESSION_LAYER_PUBLIC = 'openWebsitEexitCercleToSessionPublic';
+const EXIT_CERCLE_TO_SESSION_LAYER_PRIVATE = 'openWebsitEexitCercleToSessionPrivate';
+const EXIT_SESSION_TO_CERCLE_LAYER_PRIVATE = 'openWebsitEexitSessionToCerclePrivate';
+const EXIT_SESSION_TO_CHAPITRE_LAYER_PRIVATE = 'openWebsitEexitSessionToChapitrePrivate';
+const EXIST_TO_EVENT_LAYER = 'openWebsiteExitToEvent';
+
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().then(() => {
 
@@ -15,42 +28,57 @@ bootstrapExtra().then(() => {
             userDomain = e;
         }
     });
+
     //delete '/' caractere if exist
     if(userDomain.substring(userDomain.length -1, userDomain.length -1) === '/'){
         userDomain = userDomain.substring(0, userDomain.length -1);
     }
-    WA.state.saveVariable('domain', userDomain);
     console.log('domain', WA.state.loadVariable('domain'));
 
     const urlVideo = WA.state.loadVariable('urlVideo');
     if(urlVideo != null){
-        WA.state.saveVariable('urlVideo', `${urlVideo}/${WA.player.id}`);
+        WA.room.setProperty(VIDEO_LAYER, "openWebsite", `${userDomain}${urlVideo}/${WA.player.id}`);
+        console.log(VIDEO_LAYER, `${userDomain}${urlVideo}/${WA.player.id}`);
     }
-    console.log('urlVideo', WA.state.loadVariable('urlVideo'));
 
     const urlDocument = WA.state.loadVariable('urlDocument');
     if(urlDocument != null){
-        WA.state.saveVariable('urlDocument', `${urlDocument}/${WA.player.id}`);
+        WA.room.setProperty(DOCUMENT_LAYER, "openWebsite", `${userDomain}${urlDocument}/${WA.player.id}`);
+        console.log(DOCUMENT_LAYER, `${userDomain}${urlVideo}/${WA.player.id}`);
     }
-    console.log('urlDocument', WA.state.loadVariable('urlDocument'));
 
     const chapitreChoice = WA.state.loadVariable('chapitreChoice');
     if(chapitreChoice != null){
-        WA.state.saveVariable('chapitreChoice', `${chapitreChoice}/${WA.player.id}`);
+        WA.room.setProperty(PANNEAU_LAYER, "openWebsite", `${userDomain}${chapitreChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_CERCLE_TO_CHAPITRE_LAYER_PUBLIC, "openWebsite", `${userDomain}${chapitreChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_CERCLE_TO_CHAPITRE_LAYER_PRIVATE, "openWebsite", `${userDomain}${chapitreChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_SESSION_TO_CHAPITRE_LAYER_PRIVATE, "openWebsite", `${userDomain}${chapitreChoice}/${WA.player.id}`);
+        console.log(PANNEAU_LAYER, `${userDomain}${chapitreChoice}/${WA.player.id}`);
+        console.log(EXIT_CERCLE_TO_CHAPITRE_LAYER_PUBLIC, `${userDomain}${chapitreChoice}/${WA.player.id}`);
+        console.log(EXIT_CERCLE_TO_CHAPITRE_LAYER_PRIVATE, `${userDomain}${chapitreChoice}/${WA.player.id}`);
+        console.log(EXIT_CERCLE_TO_CHAPITRE_LAYER_PRIVATE, `${userDomain}${chapitreChoice}/${WA.player.id}`);
     }
-    console.log('chapitreChoice', WA.state.loadVariable('chapitreChoice'));
 
     const projectChoice = WA.state.loadVariable('projectChoice');
     if(projectChoice != null){
-        WA.state.saveVariable('projectChoice', `${projectChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_CHAPITRE_TO_CERCLE_LAYER_PUBLIC, "openWebsite", `${userDomain}${projectChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_CHAPITRE_TO_CERCLE_LAYER_PRIVATE, "openWebsite", `${userDomain}${projectChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_SESSION_TO_CERCLE_LAYER_PRIVATE, "openWebsite", `${userDomain}${projectChoice}/${WA.player.id}`);
+        console.log(EXIT_CHAPITRE_TO_CERCLE_LAYER_PUBLIC, `${userDomain}${projectChoice}/${WA.player.id}`);
+        console.log(EXIT_CHAPITRE_TO_CERCLE_LAYER_PRIVATE, `${userDomain}${projectChoice}/${WA.player.id}`);
+        console.log(EXIT_SESSION_TO_CHAPITRE_LAYER_PRIVATE, `${userDomain}${projectChoice}/${WA.player.id}`);
     }
-    console.log('projectChoice', WA.state.loadVariable('projectChoice'));
 
     const eventChoice = WA.state.loadVariable('eventChoice');
     if(eventChoice != null){
         WA.state.saveVariable('eventChoice', `${eventChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIST_TO_EVENT_LAYER, "openWebsite", `${userDomain}${eventChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_CERCLE_TO_SESSION_LAYER_PRIVATE, "openWebsite", `${userDomain}${eventChoice}/${WA.player.id}`);
+        WA.room.setProperty(EXIT_CERCLE_TO_SESSION_LAYER_PUBLIC, "openWebsite", `${userDomain}${eventChoice}/${WA.player.id}`);
+        console.log(EXIST_TO_EVENT_LAYER, `${userDomain}${eventChoice}/${WA.player.id}`);
+        console.log(EXIT_CERCLE_TO_SESSION_LAYER_PRIVATE, `${userDomain}${eventChoice}/${WA.player.id}`);
+        console.log(EXIT_CERCLE_TO_SESSION_LAYER_PUBLIC, `${userDomain}${eventChoice}/${WA.player.id}`);
     }
-    console.log('eventChoice', WA.state.loadVariable('eventChoice'));
 }).catch(e => console.error(e));
 
 /* #Manage premium door */
