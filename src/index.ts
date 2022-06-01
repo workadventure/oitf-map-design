8876e34228933b98dbff6c73dfd7aa22857fd6a8
Gregoire Parant <g.parant@thecodingmachine.com>
2022-06-01 14:51:35 +0200

    Update iframe opened with current url

--- src/index.ts
+++ src/index.ts
@@ -77,19 +77,19 @@ WA.onInit().then( async () => {
 
     const urlVideo = WA.state.loadVariable('urlVideo');
     if(urlVideo != null){
-        const url = `${userDomain}${urlVideo}/${WA.player.id}`;
+        const url = `${userDomain}${urlVideo}/${WA.player.id}?currentUrl=${encodeURI(window.location.href)}`;
         lissenLayer(VIDEO_LAYER, url);
     }
 
     const urlDocument = WA.state.loadVariable('urlDocument');
     if(urlDocument != null){
-        const url = `${userDomain}${urlDocument}/${WA.player.id}`;
+        const url = `${userDomain}${urlDocument}/${WA.player.id}?currentUrl=${encodeURI(window.location.href)}`;
         lissenLayer(DOCUMENT_LAYER, url);
     }
 
     const chapitreChoice = WA.state.loadVariable('chapitreChoice');
     if(chapitreChoice != null){
-        const url = `${userDomain}${chapitreChoice}/${WA.player.id}`;
+        const url = `${userDomain}${chapitreChoice}/${WA.player.id}?currentUrl=${encodeURI(window.location.href)}`;
         lissenLayer(PANNEAU_LAYER, url);
         lissenLayer(EXIT_CERCLE_TO_CHAPITRE_LAYER_PUBLIC, url);
         lissenLayer(EXIT_CERCLE_TO_CHAPITRE_LAYER_PRIVATE, url);
@@ -98,7 +98,7 @@ WA.onInit().then( async () => {
 
     const projectChoice = WA.state.loadVariable('projectChoice');
     if(projectChoice != null){
-        const url = `${userDomain}${projectChoice}/${WA.player.id}`;
+        const url = `${userDomain}${projectChoice}/${WA.player.id}?currentUrl=${encodeURI(window.location.href)}`;
         lissenLayer(EXIT_CHAPITRE_TO_CERCLE_LAYER_PUBLIC, url);
         lissenLayer(EXIT_CHAPITRE_TO_CERCLE_LAYER_PRIVATE, url);
         lissenLayer(EXIT_SESSION_TO_CERCLE_LAYER_PRIVATE, url);
@@ -106,7 +106,7 @@ WA.onInit().then( async () => {
 
     const eventChoice = WA.state.loadVariable('eventChoice');
     if(eventChoice != null){
-        const url = `${userDomain}${eventChoice}/${WA.player.id}`;
+        const url = `${userDomain}${eventChoice}/${WA.player.id}?currentUrl=${encodeURI(window.location.href)}`;
         lissenLayer(EXIST_TO_EVENT_LAYER, url);
         lissenLayer(EXIT_CERCLE_TO_SESSION_LAYER_PRIVATE, url);
         lissenLayer(EXIT_CERCLE_TO_SESSION_LAYER_PUBLIC, url);
