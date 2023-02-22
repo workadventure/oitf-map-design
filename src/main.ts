0fc036e42ee018cfa3375e49d0fa145347d56da0
Valdo <vteixeiraromao@gmail.com>
2023-02-22 22:05:16 +0000

    Added doors to all maps + updated script

--- src/main.ts
+++ src/main.ts
@@ -1,154 +1,155 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra"
 
-console.log('Script started successfully');
+console.log('Script started successfully')
 
-const VIDEO_LAYER = 'Video';
-const DOCUMENT_LAYER = 'Document';
-const PANNEAU_LAYER = 'urlPanneau';
-const EXIT_CHAPITRE_TO_CERCLE_LAYER_PUBLIC = 'openWebsitEexitChapitreToCerclePublic';
-const EXIT_CHAPITRE_TO_CERCLE_LAYER_PRIVATE = 'openWebsitEexitChapitreToCerclePrivate';
-const EXIT_CERCLE_TO_CHAPITRE_LAYER_PUBLIC = 'openWebsitEexitCercleToChapitrePublic';
-const EXIT_CERCLE_TO_CHAPITRE_LAYER_PRIVATE = 'openWebsitEexitCercleToChapitrePrivate';
-const EXIT_CERCLE_TO_SESSION_LAYER_PUBLIC = 'openWebsitEexitCercleToSessionPublic';
-const EXIT_CERCLE_TO_SESSION_LAYER_PRIVATE = 'openWebsitEexitCercleToSessionPrivate';
-const EXIT_SESSION_TO_CERCLE_LAYER_PRIVATE = 'openWebsitEexitSessionToCerclePrivate';
-const EXIT_SESSION_TO_CHAPITRE_LAYER_PRIVATE = 'openWebsitEexitSessionToChapitrePrivate';
-const EXIST_TO_EVENT_LAYER = 'openWebsiteExitToEvent';
+// Public links
+const VIDEO_LAYER = 'Video'
+const DOCUMENT_LAYER = 'Document'
+const PANNEAU_LAYER = 'urlPanneau'
 
-/* Manage premium door */
-const DOOR_CLOSED_LAYER_NAME = 'doorClosed';
-const DOOR_OPENED_LAYER_NAME = 'doorOpened';
+// Event exit
+const EXIT_TO_EVENT_LAYER = 'ToEvent'
+
+// Cercle exits
+const EXIT_CHAPITRE_TO_CERCLE_PUBLIC = 'ChapitreToCerclePublic'
+const EXIT_CHAPITRE_TO_CERCLE_PRIVATE = 'ChapitreToCerclePrivate'
+
+const EXIT_CERCLE_TO_CHAPITRE_PUBLIC = 'CercleToChapitrePublic'
+const EXIT_CERCLE_TO_CHAPITRE_PRIVATE = 'CercleToChapitrePrivate'
+
+// Session exits
+const EXIT_CERCLE_TO_SESSION_PUBLIC = 'CercleToSessionPublic'
+const EXIT_CERCLE_TO_SESSION_PRIVATE = 'CercleToSessionPrivate'
+
+const EXIT_SESSION_TO_CERCLE_PRIVATE = 'SessionToCerclePrivate'
+const EXIT_SESSION_TO_CHAPITRE_PRIVATE = 'SessionToChapitrePrivate'
+
+// Door states wording
+const PREMIUM_DOOR = 'premiumDoor'
+const PUBLIC_DOOR = 'publicDoor'
+const MILAN_DOOR = 'milanDoor'
+const JOHANNESBOURG_DOOR = 'johannesbourgDoor'
 
 // Waiting for the API to be ready
 WA.onInit().then(async () => {
-    console.log('Scripting API ready');
+    console.log('Scripting API ready')
     console.log('Player tags: ', WA.player.tags)
 
-    //close the door
-    closeDoor();
-
-    //subscribe to open the door if user is premium
-    WA.room.onEnterLayer('doorZone').subscribe(toggleDoor);
-
-    console.info('Init WA OITF');
-
-    //define the domain
-    let userDomain: string = '';
-    if (WA.state.hasVariable('domain')) {
-        userDomain = (WA.state.loadVariable('domain') as string);
+    // Manage the doors
+    closeDoor(PUBLIC_DOOR)
+    closeDoor(PREMIUM_DOOR)
+    closeDoor(MILAN_DOOR)
+    closeDoor(JOHANNESBOURG_DOOR)
+    listenDoor(PUBLIC_DOOR)
+    if (isPremium()) {
+        listenDoor(PREMIUM_DOOR)
+        listenDoor(MILAN_DOOR)
+        listenDoor(JOHANNESBOURG_DOOR)
     }
 
-    WA.player.tags.forEach((e: string) => {
-        console.log(e);
-        if (e.match(/http(?:s)?:\/\/(?:[\w-]+\.)*([\w-]{1,63})(?:\.(?:\w{3}|\w{2,10}))(?:$|\/)/i) != null) {
-            userDomain = e;
+    // Manage the domain
+    let userDomain: string = ''
+    if (WA.state.hasVariable('domain')) {
+        userDomain = (WA.state.loadVariable('domain') as string)
+    }
+    WA.player.tags.forEach((tag: string) => {
+        if (tag.match(/http(?:s)?:\/\/(?:[\w-]+\.)*([\w-]{1,63})(?:\.(?:\w{3}|\w{2,10}))(?:$|\/)/i) != null) {
+            userDomain = tag
         }
-    });
-
+    })
     if (userDomain !== '') {
         //delete '/' caractere if exist
         if (userDomain.substring(userDomain.length - 1, userDomain.length - 1) === '/') {
-            userDomain = userDomain.substring(0, userDomain.length - 1);
+            userDomain = userDomain.substring(0, userDomain.length - 1)
         }
     }
 
-    const urlVideo = WA.state.loadVariable('urlVideo');
+    const urlVideo = WA.state.loadVariable('urlVideo')
     if (urlVideo != null) {
-        const url = `${userDomain}${urlVideo}/${WA.player.id}?currentUrl=${WA.room.id}`;
-        lissenLayer(VIDEO_LAYER, url);
+        const url = `${userDomain}${urlVideo}/${WA.player.uuid}?currentUrl=${WA.room.id}`
+        listenLayer(VIDEO_LAYER, url)
     }
 
-    const urlDocument = WA.state.loadVariable('urlDocument');
+    const urlDocument = WA.state.loadVariable('urlDocument')
     if (urlDocument != null) {
-        const url = `${userDomain}${urlDocument}/${WA.player.id}?currentUrl=${WA.room.id}`;
-        lissenLayer(DOCUMENT_LAYER, url);
+        const url = `${userDomain}${urlDocument}/${WA.player.uuid}?currentUrl=${WA.room.id}`
+        listenLayer(DOCUMENT_LAYER, url)
     }
 
-    const chapitreChoice = WA.state.loadVariable('chapitreChoice');
+    const chapitreChoice = WA.state.loadVariable('chapitreChoice')
     if (chapitreChoice != null) {
-        const url = `${userDomain}${chapitreChoice}/${WA.player.id}?currentUrl=${WA.room.id}`;
-        lissenLayer(PANNEAU_LAYER, url);
-        lissenLayer(EXIT_CERCLE_TO_CHAPITRE_LAYER_PUBLIC, url);
-        lissenLayer(EXIT_CERCLE_TO_CHAPITRE_LAYER_PRIVATE, url);
-        lissenLayer(EXIT_SESSION_TO_CHAPITRE_LAYER_PRIVATE, url);
+        const url = `${userDomain}${chapitreChoice}/${WA.player.uuid}?currentUrl=${WA.room.id}`
+        listenLayer(PANNEAU_LAYER, url)
+        listenLayer(EXIT_CERCLE_TO_CHAPITRE_PUBLIC, url)
+        listenLayer(EXIT_CERCLE_TO_CHAPITRE_PRIVATE, url)
+        listenLayer(EXIT_SESSION_TO_CHAPITRE_PRIVATE, url)
     }
 
-    const projectChoice = WA.state.loadVariable('projectChoice');
+    const projectChoice = WA.state.loadVariable('projectChoice')
     if (projectChoice != null) {
-        const url = `${userDomain}${projectChoice}/${WA.player.id}?currentUrl=${WA.room.id}`;
-        lissenLayer(EXIT_CHAPITRE_TO_CERCLE_LAYER_PUBLIC, url);
-        lissenLayer(EXIT_CHAPITRE_TO_CERCLE_LAYER_PRIVATE, url);
-        lissenLayer(EXIT_SESSION_TO_CERCLE_LAYER_PRIVATE, url);
+        const url = `${userDomain}${projectChoice}/${WA.player.uuid}?currentUrl=${WA.room.id}`
+        listenLayer(EXIT_CHAPITRE_TO_CERCLE_PUBLIC, url)
+        listenLayer(EXIT_CHAPITRE_TO_CERCLE_PRIVATE, url)
+        listenLayer(EXIT_SESSION_TO_CERCLE_PRIVATE, url)
     }
 
-    const eventChoice = WA.state.loadVariable('eventChoice');
+    const eventChoice = WA.state.loadVariable('eventChoice')
     if (eventChoice != null) {
-        const url = `${userDomain}${eventChoice}/${WA.player.id}?currentUrl=${WA.room.id}`;
-        lissenLayer(EXIST_TO_EVENT_LAYER, url);
-        lissenLayer(EXIT_CERCLE_TO_SESSION_LAYER_PRIVATE, url);
-        lissenLayer(EXIT_CERCLE_TO_SESSION_LAYER_PUBLIC, url);
+        const url = `${userDomain}${eventChoice}/${WA.player.uuid}?currentUrl=${WA.room.id}`
+        listenLayer(EXIT_TO_EVENT_LAYER, url)
+        listenLayer(EXIT_CERCLE_TO_SESSION_PRIVATE, url)
+        listenLayer(EXIT_CERCLE_TO_SESSION_PUBLIC, url)
     }
 
-    //Embeded website video
-    const website = await WA.room.website.get("iframeVideoAutiroriumYoutube");
+    // Embeded website video
+    const website = await WA.room.website.get("iframeVideoAuditoriumYoutube")
     if (website != undefined) {
-        //define Youtube video in variable
-        if (WA.state.hasVariable('urlVideoAutiroriumYoutube')) {
-            website.url = (WA.state.loadVariable('urlVideoAutiroriumYoutube') as string);
+        // define Youtube video in variable
+        if (WA.state.hasVariable('urlVideoAuditoriumYoutube')) {
+            website.url = (WA.state.loadVariable('urlVideoAuditoriumYoutube') as string)
         }
         //subscribe change
-        WA.state.onVariableChange('urlVideoAutiroriumYoutube').subscribe(() => {
-            website.url = (WA.state.loadVariable('urlVideoAutiroriumYoutube') as string);
-        });
-
-        /*focus camera on the video when user enter in the zone
-        WA.room.onEnterLayer('AutiroriumYoutube').subscribe(() => {
-            WA.camera.set(
-                website.x,
-                website.y,
-                website.width,
-                website.height,
-                true,
-                true
-            );
-        });
-        //unfocus camera when user leave the zone
-        WA.room.onLeaveLayer('AutiroriumYoutube').subscribe(() =>{
-            WA.camera.followPlayer();
-        });*/
+        WA.state.onVariableChange('urlVideoAuditoriumYoutube').subscribe(() => {
+            website.url = (WA.state.loadVariable('urlVideoAuditoriumYoutube') as string)
+        })
     }
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-}).catch(e => console.error(e));
+        console.log('Scripting API Extra ready')
+    }).catch(e => console.error(e))
+}).catch(e => console.error(e))
 
-const closeDoor = () => {
-    WA.room.showLayer(DOOR_CLOSED_LAYER_NAME);
-    WA.room.hideLayer(DOOR_OPENED_LAYER_NAME);
-}
-const openDoor = () => {
-    WA.room.hideLayer(DOOR_CLOSED_LAYER_NAME);
-    WA.room.showLayer(DOOR_OPENED_LAYER_NAME);
-}
 const isPremium = () => {
     return WA.player.tags.includes('premium') || WA.player.tags.includes('Premium')
-        || WA.player.tags.includes('PREMIUM') || WA.player.tags.includes('editor');
-}
-const toggleDoor = () => {
-    console.log('isPremium()', isPremium());
-    if (isPremium()) {
-        openDoor();
-    } else {
-        closeDoor();
-    }
+        || WA.player.tags.includes('PREMIUM') || WA.player.tags.includes('editor')
 }
 
-const lissenLayer = (layer: string, url: string) => {
+const listenLayer = (layer: string, url: string) => {
     WA.room.onEnterLayer(layer).subscribe(() => {
         WA.nav.openCoWebSite(url, true, 'fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; midi; camera; microphone;')
-    });
-    WA.room.onLeaveLayer(layer).subscribe(() => WA.nav.closeCoWebSite());
+    })
+    WA.room.onLeaveLayer(layer).subscribe(() => WA.nav.closeCoWebSite())
+}
+
+const listenDoor = (layer: string) => {
+    WA.room.area.onEnter(layer).subscribe(() => {
+        openDoor(layer)
+    })
+    WA.room.area.onLeave(layer).subscribe(() => {
+        closeDoor(layer)
+    })
+}
+
+const openDoor = (layer: string) => {
+    console.log('open door', layer)
+    WA.room.hideLayer(layer + "Closed")
+    WA.room.showLayer(layer + "Open")
+}
+
+const closeDoor = (layer: string) => {
+    console.log('close door', layer)
+    WA.room.hideLayer(layer + "Open")
+    WA.room.showLayer(layer + "Closed")
 }
